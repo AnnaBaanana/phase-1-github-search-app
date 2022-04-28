dfdace2c5bd5628b745a1286dee2d2f111db0f53
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-28 16:25:16 -0400

    adding the search param and fetching but not displaying

--- js/index.js
+++ js/index.js
@@ -1,15 +1,8 @@
 const userSearchURL = "https://api.github.com/search/users?q="
-let param = "Vanya"
-const form = document.querySelector("#github-form")
+let param;
+//const form = document.querySelector("#github-form")
+//console.log(form)
 
-domLoaded()
-
-function domLoaded() {
-    document.addEventListener('DOMContentLoaded', ()=> {
-    console.log("DOM Loaded")
-    renderData()
-    })
-}
 
 function renderData() {
     console.log(`${userSearchURL}+${param}`)
@@ -17,17 +10,41 @@ function renderData() {
         headers: {
             "Accept": "application/vnd.github.v3+json"
         }
-    }).then(res=> res.json()).then(data => {
-        console.log(data)
-        searchUser()
+    }).then(res => res.json()).then(data => {
+        console.log(data.items)
+        const userData = data.items.forEach((element) => {
+            console.log(element.login)
+            let userList = document.querySelector("#user-list")
+            const div = document.createElement("li")
+            const h4 = document.createElement("h4")
+            h4.textContent = element.login
+            const img = document.createElement("img")
+            img.className = "avatar"
+            img.src = element.avatar_url
+            const p = document.createElement("p")
+            div.append(h4, img)
+            userList.append(div)
+        })
     })
 }
 
 function searchUser() {
+    const form = document.querySelector("#github-form")
     console.log(form)
     form.addEventListener('submit', (e)=> {
         e.preventDefault()
-        console.log(e)
+        console.log(e.target[0].value)
+        param = e.target[0].value
+        renderData()
     })
 }
 
+function domLoaded() {
+    document.addEventListener('DOMContentLoaded', ()=> {
+    console.log("DOM Loaded")
+    renderData()
+    searchUser()
+    })
+}
+
+domLoaded()
